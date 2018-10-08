4f1d461b8f23433ec7b5c5646d05cdd1bc43a4e7
csteyrer <christian.steyrer@hotmail.com>
2018-10-08 20:02:29 +0200

    2018-09-25 / K04

---
+++ RestPrimer-St/src/main/java/at/htl/restprimer/TimeServerResourceIT.java
@@ -0,0 +1,12 @@
+package at.htl.restprimer;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("rs")
+public class TimeServerResourceIT {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
